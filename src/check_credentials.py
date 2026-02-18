311c28f65644eb54bc8e8101c152ff229b5cfdc7
malikatapparel <tapparelmalika@gmail.com>
2026-02-18 09:36:10 +0100

    Update check_credentials.py

--- src/check_credentials.py
+++ src/check_credentials.py
@@ -1,12 +1,19 @@
 # For production, you would set these as environment variables and read them like this:
 import argparse
 from email.message import EmailMessage
-parser = argparse.ArgumentParser(description='Check credentials for PubMed Weekly Recommendation System')
-parser.add_argument('--SMTP_USER', required=True, help='SMTP username for sending emails')
-parser.add_argument('--SMTP_PASS', required=True, help='SMTP password for sending emails')
-parser.add_argument('--SMTP_RECEIVER', required=True, help='Email address to receive the recommendations')
-parser.add_argument('--PUBMED_API_KEY', required=True, help='API key for accessing PubMed data')
+# Get credentials from environment variables
+
+parser = argparse.ArgumentParser(description='PubMed Weekly Recommendation System')
+parser.add_argument('--smtp_user', required=True, help='SMTP username for sending emails')
+parser.add_argument('--smtp_pass', required=True, help='SMTP password for sending emails')
+parser.add_argument('--receiver', required=True, help='Email address to receive the recommendations')
+parser.add_argument('--pubmed_api_key', required=True, help='API key for accessing PubMed data')
 args = parser.parse_args()
+
+smtp_user = args.smtp_user
+smtp_pass = args.smtp_pass
+receiver = args.receiver
+pubmed_api_key = args.pubmed_api_key
 
 
 # Send email
